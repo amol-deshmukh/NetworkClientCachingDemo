1456dc1cd8d4d00f73c330be42a2457974cef0fb
Amol Deshmukh <amold86@gmail.com>
2019-02-14 12:27:09 +0530

    Migrated to android x

--- app/src/main/java/com/ghatnandurkar/networkclientcachingdemo/MainActivity.java
+++ app/src/main/java/com/ghatnandurkar/networkclientcachingdemo/MainActivity.java
@@ -1,6 +1,6 @@
 package com.ghatnandurkar.networkclientcachingdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/ghatnandurkar/networkclientcachingdemo/network/RetrofitProvider.java
@@ -0,0 +1,10 @@
+package com.ghatnandurkar.networkclientcachingdemo.network;
+
+/**
+ * Created by
+ *
+ * @author Amol Deshmukh
+ * @since 14/02/19
+ */
+public class RetrofitProvider {
+}
